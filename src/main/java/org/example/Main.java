b2dcea54a16953fe4805647ca575c009186cb2b9
Estella1 <estella.santos.moreira@gmail.com>
2025-08-05 15:02:43 -0300

    so pra fazer push

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -7,6 +7,7 @@ import javax.swing.*;
 public class Main {
 
     public static void main(String[] args) {
+        //so pra
         SwingUtilities.invokeLater(() -> new TelaPrincipal().setVisible(true));
 
         // Garante que a interface gráfica seja executada na thread de eventos da AWT (EDT)
