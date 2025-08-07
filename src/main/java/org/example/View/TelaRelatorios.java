0a2351297e30407a622514666330270bdd56de9f
Estella1 <estella.santos.moreira@gmail.com>
2025-08-07 20:36:40 -0300

    acrescentou botão necessidade

--- src/main/java/org/example/View/TelaRelatorios.java
+++ src/main/java/org/example/View/TelaRelatorios.java
@@ -9,16 +9,20 @@ public class TelaRelatorios extends javax.swing.JFrame {
         setSize(300, 200);
         setLocationRelativeTo(null);
         setDefaultCloseOperation(DISPOSE_ON_CLOSE);
-        setLayout(new GridLayout(3, 1, 10, 10));
+        setLayout(new GridLayout(4, 1, 10, 10));
 
         JButton btnMediaDoacoes = new JButton("Relatório de Media de Doações");
         btnMediaDoacoes.addActionListener(e -> new TelaRelatorioMediaDoacoes().setVisible(true));
         JButton btnRelatorioIdade = new JButton("Relatório de Idade");
         btnRelatorioIdade.addActionListener(e -> new TelaRelatorioIdade().setVisible(true));
 
+        JButton btnRelatorioNecessidades = new JButton("Relatorio de Necessidades");
+        btnRelatorioNecessidades.addActionListener(e -> new TelaRelatorioNecessidades().setVisible(true));
+
         add(btnMediaDoacoes);
         setVisible(true);
         add(btnRelatorioIdade);
         setVisible(true);
+        add(btnRelatorioNecessidades);
     }
 }
